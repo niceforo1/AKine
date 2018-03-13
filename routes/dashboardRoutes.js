e6e7ff2f022bf6e70825ca9201617590c19f2a36
David Campana <david.campana87@gmail.com>
2018-03-12 23:40:09 -0300

    Se suben cambios en DashboardRoutes para grafico

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,17 +1,39 @@
 module.exports = (app, mongoose) => {
-  const Patient = mongoose.model('patient');
-  const Professional = mongoose.model('professional');
+    const Patient = mongoose.model('patient');
+    const Professional = mongoose.model('professional');
 
-  app.get('/api/dashboard-home', async (req, res) => {
-    try {
-      const patient = await Patient.count({});
-      const professional = await Professional.count({});
-      let result = { 'professionalCountTotal': professional, 'patientCountTotal': patient };
-      res.send(result);
-    } catch (err) {
-      res.status(500).send(err);
+    app.get('/api/dashboard/home', async (req, res) => {
+        try {
+
+            const patient = await Patient.count({});
+            const professional = await Professional.count({});
+            let result = {
+                'professionalCountTotal': professional, 'patientCountTotal': patient,
+                'patientInsurances': [
+                    {
+                        'name': 'Sancor',
+                        'value': 3
+                    },
+                    {
+                        'name': 'Pami',
+                        'value': 12
+                    },
+                    {
+                        'name': 'Osecac',
+                        'value': 4
+                    },
+                    {
+                        'name': 'Osde',
+                        'value': 6
+                    }
+                ]
+            };
+
+            res.send(result);
+        }
+        catch(err) {
+            res.status(500).send(err);
+        }
     }
-  });
-
-
+    );
 };
