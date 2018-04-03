975dcb0cb281c17bbac59f6fdf3a5055591c535c
Chapadiex <exequielsantoro@gmail.com>
2018-04-02 21:35:59 -0300

    ES - Se modifico en la API DASHBOARD (dashboardRoutes.js) : $groups

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,3 +1,4 @@
+
 module.exports = (app, mongoose) => {
     const Patient = mongoose.model('patient');
     const Professional = mongoose.model('professional');
@@ -37,3 +38,40 @@ module.exports = (app, mongoose) => {
     }
     );
 };
+
+/*
+module.exports = (app, mongoose) => {
+    const Patient = mongoose.model('patient');
+    const Professional = mongoose.model('professional');
+
+    app.get('/api/dashboard/home', async (req, res) => {
+            try {
+
+                const patient = await Patient.count({});
+                const professional = await Professional.count({});
+                const pacientesPorObSoc = await Patient.aggregate({
+                        $group:
+                            {
+                                _id : {
+                                    id : "$socialInsurance._id",
+                                    names : "$socialInsurance.number"
+                                },
+                                value : { $sum: 1 }
+                            }
+                    }
+                );
+                let result = {
+                    'professionalCountTotal': professional,
+                    'patientCountTotal': patient,
+                    'patientInsurances': pacientesPorObSoc
+                };
+
+                res.send(result);
+            }
+            catch(err) {
+                res.status(500).send(err);
+            }
+        }
+    );
+};
+*/
