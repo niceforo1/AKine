69c43c8e3ad6ab9b78baf4477c580507c4acff31
Chapadiex <exequielsantoro@gmail.com>
2018-04-05 16:20:18 -0300

    ES: dashboardRoutes.js se agrego a la llamada de Pacientes por Obra Sociales  para el grafico

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -2,76 +2,35 @@
 module.exports = (app, mongoose) => {
     const Patient = mongoose.model('patient');
     const Professional = mongoose.model('professional');
+    const SocialInsurance = mongoose.model('socialInsurance');
 
     app.get('/api/dashboard/home', async (req, res) => {
         try {
-
             const patient = await Patient.count({});
             const professional = await Professional.count({});
+            const pacientesPorObSoc = await Patient.aggregate({
+                $group: {
+                    _id: '$socialInsurance._id',
+                    count: { $sum: 1 }
+                }
+            });
+            const arr = pacientesPorObSoc.map(async (item, index) => {
+                const socIns = await SocialInsurance.findOne({ _id: item._id });
+                return {
+                    _id: socIns._id,
+                    name: socIns.name,
+                    value: item.count
+                };
+            });
             let result = {
-                'professionalCountTotal': professional, 'patientCountTotal': patient,
-                'patientInsurances': [
-                    {
-                        'name': 'Sancor',
-                        'value': 3
-                    },
-                    {
-                        'name': 'Pami',
-                        'value': 12
-                    },
-                    {
-                        'name': 'Osecac',
-                        'value': 4
-                    },
-                    {
-                        'name': 'Osde',
-                        'value': 6
-                    }
-                ]
+                professionalCountTotal: professional,
+                patientCountTotal: patient,
+                patientInsurances: await Promise.all(arr)
             };
-
             res.send(result);
-        }
-        catch(err) {
+
+        } catch (err) {
             res.status(500).send(err);
         }
-    }
-    );
+    });
 };
-
-/*
-module.exports = (app, mongoose) => {
-    const Patient = mongoose.model('patient');
-    const Professional = mongoose.model('professional');
-
-    app.get('/api/dashboard/home', async (req, res) => {
-            try {
-
-                const patient = await Patient.count({});
-                const professional = await Professional.count({});
-                const pacientesPorObSoc = await Patient.aggregate({
-                        $group:
-                            {
-                                _id : {
-                                    id : "$socialInsurance._id",
-                                    names : "$socialInsurance.number"
-                                },
-                                value : { $sum: 1 }
-                            }
-                    }
-                );
-                let result = {
-                    'professionalCountTotal': professional,
-                    'patientCountTotal': patient,
-                    'patientInsurances': pacientesPorObSoc
-                };
-
-                res.send(result);
-            }
-            catch(err) {
-                res.status(500).send(err);
-            }
-        }
-    );
-};
-*/
